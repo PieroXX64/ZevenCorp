df13a27d1604945a1216a4c2d37dcbd990605d72
PieroXX64 <pieroo_416@hotmail.com>
2025-06-29 22:54:49 -0500

    app.py

--- app.py
+++ app.py
@@ -177,4 +177,3 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Usar el puerto dinámico proporcionado por Render
     app.run(debug=True, host="0.0.0.0", port=port)  # Escuchar en todas las interfaces de red
 
-
